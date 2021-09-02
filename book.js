96069cb592282627f4327a9980e06acbc455f08f
MasudSultan4 <mdsarwarjahanmasudsultan1971@gmail.com>
2021-09-02 20:24:22 +0600

    last commit

--- book.js
+++ book.js
@@ -1,93 +1,58 @@
-// search btn 
-const searchBtn = document.getElementById('search-btn')
-
-// spinner
+// This function for load data
+const loadData = () => {
+    const inputValue = document.getElementById('inputField');
+    const inputText = inputValue.value;
+    // spinner
 const spinner = document.getElementById('spinner');
-// book container 
-const bookContainer = document.getElementById('book-container')
 
-searchBtn.addEventListener('click',loadBook)
-
-
-function loadBook(){
-    const errorDiv = document.getElementById("error");
-    errorDiv.innerHTML = "";
-    const searchTextInput = document.getElementById("search-text");
-    const searchText = searchTextInput.value;
-
-    // Handale empty search 
-    if (searchText == 0) {
-        errorDiv.innerHTML = `<h4 class="text-center">Search Field Cannot be Empty</h4>
-        `;
+    // this condition for checking valid input data
+    if (inputText.length > 0) {
+       spinner.classList.remove('d-none')
+        const url = `https://openlibrary.org/search.json?q=${inputText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayData(data.docs, data.numFound))
+            .finally(() => spinner.classList.add('d-none'))
     }
     else {
-        spinner.classList.remove('d-none')
-        fetch(`https://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => displayBook(data.docs))
-        .finally(() => spinner.classList.add('d-none'))
+        document.getElementById('countResult').innerText = `Search fild cannot be empty`;
+        document.getElementById('displayField').textContent = ''
+    }
+    inputValue.value = ''
+    document.getElementById('countResult').innerText = '';
+}
+
+// This Function Show Data in UI
+const displayData = (books, totalDataFound) => {
+    const displayField = document.getElementById('displayField')
+    displayField.textContent = ''
+    const countResult = document.getElementById('countResult')
+
+    // it will validate data caount
+    if (totalDataFound === 0) {
+        countResult.innerText = 'No Result Found'
+    }
+    else {
+        books.forEach(book => {
+            // destrucuring from object
+            const { title, author_name, first_publish_year, publisher, cover_i } = book;
+            const imgUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`
+            const displayCard = document.createElement('div')
+            displayCard.classList.add('col')
+            displayCard.innerHTML = `
+                <div class="card cardHeight">
+                    <img src="${imgUrl}" class="card-img-top img-fluid imgSize" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${title.slice(0, 30)}</h5>
+                    <p>Author Name: <b>${author_name && author_name.slice(0,1)? author_name && author_name.slice(0,1): author_name}</b></b>
+                    <p>First Publish: <b>${first_publish_year}</b></b>
+                    <p>Publisher: <b>${publisher && publisher.slice(0,1)? publisher && publisher.slice(0,1): publisher}</b></b>
+                    </div>
+                </div>
+        `
+            displayField.appendChild(displayCard)
+        });
+        countResult.innerHTML = `<h3 class="text-success">We show you ${books.length} results of ${totalDataFound}</h3>`
     }
 
-    searchTextInput.value = "";
 }
-
-// Show Result 
-const showResult = data => {
-
-  const searchResultDiv = document.getElementById("error");
-  searchResultDiv.innerHTML = '';
-
-
-  if (data > 0) {
-      searchResultDiv.innerHTML = ` <h2>Search Result : ${data.length} items </h2>`
-  }
-  else {
-      searchResultDiv.innerHTML = ` <h3>No Items Found </h3>`
-  }
-}
-
-
-
-
-const displayBook = data => {
-
-  // Serch Item Result Here
-  showResult(data.docs)
-
-   data.forEach(book =>{
-        console.log(book)
-        const{title,author_name,first_publish_year,publisher} = book;
-
-        const notFound = "Not-Found";
-        const div = document.createElement('div');
-        div.classList.add('col-md-3');
-        
-        div.innerHTML = `
-        <div class="rounded overflow-hidden border p-2">
-      <img
-        src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
-        class="w-100"
-        alt=""
-      />
-    </div>
-    
-    <div
-      class="
-        py-2
-        d-flex
-        justify-content-between
-        align-items-center
-        d-md-block
-        text-md-center
-      "
-    >
-      <h5>Book-name: <span class="text-info">${title?title:notFound}</span></h5>
-      <p>Author-name: <span class="text-info">${author_name?author_name:notFound}</span> </p>
-      <h5>Publish-year: <span class="text-info">${first_publish_year?first_publish_year:notFound}</span></h5>
-      <p>Publisher: <span class="text-info">${publisher?publisher:notFound}</span></p>
-    </div>
-  `
-  bookContainer.appendChild(div)
-    })
-  
-  }
